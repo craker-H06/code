288c827ba8db4980a1c98d9f519a6f799144ba1c
sudais <mhsudais66@gmail.com>
2025-03-18 00:52:44 +0100

    Initialisation du projet

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// cette page gere mon appli
+// dom selection
+const list = 'element #list'
+const input = 'element #input'
+const add = 'element #add '
+const clear = 'element #clear'
+const url = 'element #url'
+const load = 'element #load'
